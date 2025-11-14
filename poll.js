bfa844c8ad5b1c411c15dc5a19038a613b61e614
2Smart-dev-debug <caydenblaze21@gmail.com>
2025-11-13 20:39:04 -0500

    Delete poll.js

--- poll.js
+++
@@ -1,6 +0,0 @@
-let currentPoll = global.currentPoll || null;
-
-export default function handler(req, res) {
-    global.currentPoll = currentPoll;
-    res.status(200).json(currentPoll || { active: false });
-}
